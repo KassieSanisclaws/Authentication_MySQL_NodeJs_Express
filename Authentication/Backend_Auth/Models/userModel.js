6fe37a92c072d7f5645ffa4530c5116c5bad9e64
KassieSanisclaws <86675041+KassieSanisclaws@users.noreply.github.com>
2021-07-19 09:57:20 -0700

    Add files via upload

--- Authentication/Backend_Auth/Models/userModel.js
+++ Authentication/Backend_Auth/Models/userModel.js
@@ -1,13 +1,9 @@
-const dbconnect = require("../../config/config.db");
+const dbconnect = require('../Config/config.db');
 
-const User = function(user) {
-     this.user_firstname  = user.user_firstname;
-     this.user_lastname   = user.user_lastname;
-     this.user_email      = user.user_email;
-     this.user_password   = user.user_password;
-     this.created_at  = newDate();
-     this.updated_at  = newDate();
-};
+const User = function(user){
+    this.user_email     = user.user_email;
+    this. user_password = user.user_password;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // getUsersList MySQL query string to retreive from database. //
@@ -23,8 +19,8 @@ User.getUsersList = (result) =>{
     });
 };
 
-// getUserByID MySQL query to retreive from database.(working)//
-User.getUserByID = (id, result) => {
+// get user by ID from data base. (working)//
+User.getUserByID = (id, result)=>{
     dbconnect.query('SELECT * FROM users WHERE user_id=?', id, (err, res)=>{
         if(err){
             console.log("Error fetching users by id", err);
@@ -32,12 +28,12 @@ User.getUserByID = (id, result) => {
         }else{
             result(null, res);
         }
-    });
-};
+    })
+}
 
 // createNewUser MySQL query to creating a new user.// 
 User.createNewUser = (userReqData, result) =>{
-    dbconnect.query('INSERT INTO authentication.users SET ?', users, function (error, result, fields) {
+    dbconnect.query('INSERT INTO authentication.users (email, password)?', users, function (error, result, fields) {
         if (error) {
             res.send({
               "code":400,
@@ -53,8 +49,22 @@ User.createNewUser = (userReqData, result) =>{
 }
 
 // loginUser MySQL query string to login user from database validation. //
+User.loginUser = (req, result) => {
+    dbconnect.query('SELECT * FROM authentication.users WHERE (user_email, user_password) LIKE (user_email, user_password)?', users, function (error, result, fields) {
+        if (error) {
+            res.send({
+              "code":400,
+              "failed":"error ocurred"
+            })
+          } else {
+            res.send({
+              "code":200,
+              "success":"user loggedin sucessfully"
+                });
+            } 
+    })
+}
 
 
-        
 module.exports = User;
 
