c29621f87e3c69e96c7eacc5493c2073ee0a0866
KassieSanisclaws <86675041+KassieSanisclaws@users.noreply.github.com>
2021-07-27 09:14:16 -0700

    Add files via upload

---
+++ Frontend/Authentication/Components/Navbar/NavbarElements.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+background: #51CD91;
+height: 80px;
+width: 120rem;
+margin-left: -3rem;
+margin-top: -8px; 
+justify-content: center;
+align-items: center;
+font-size: 1rem;
+top: 0;
+z-index: 10;
+
+@media screen and (max-width: 980px){
+    transition; 0.8s allease;
+}
+`;
+
