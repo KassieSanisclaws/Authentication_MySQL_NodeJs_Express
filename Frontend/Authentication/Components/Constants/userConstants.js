6dea5bdd94e7aada448dd830916b2571df212870
KassieSanisclaws <86675041+KassieSanisclaws@users.noreply.github.com>
2021-07-27 09:11:56 -0700

    Add files via upload

---
+++ Frontend/Authentication/Components/Constants/userConstants.js
@@ -0,0 +1,15 @@
+// user register constant actions. //
+export const USER_REGISTER_REQUEST = 'USER_REGISTER_REQUEST';
+export const USER_REGISTER_SUCCESS = 'USER_REGISTER_SUCCESS';
+export const USER_REGISTER_FAIL = 'USER_REGISTER_FAIL';
+
+// user login constant actions. //
+export const USER_LOGIN_REQUEST = 'USER_LOGIN_REQUEST';
+export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS';
+export const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL';
+
+// user log out constant action. //
+export const USER_LOGOUT = 'USER_LOGOUT';
+
+// user details constant action. // 
+
